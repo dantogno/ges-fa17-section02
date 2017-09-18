0f0ad82d0eaecb1410a20b093090e6c9cc653a63
David Antognoli <vebulidbih@msn.com>
2017-09-18 15:18:55 -0500

    followed along with in class demo...

---
+++ GES 2D Demo section 02/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	// Use this for initialization
+	void Start ()
+    {
+        //Debug.Log("Called from Start.");  
+
+        // This code "teleports" my game object to a new position
+        //transform.position = new Vector3(3,0,0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Debug.Log("horizontal input: " + horizontalInput);
+
+        float speed = 0.2f;
+
+        transform.Translate(speed * horizontalInput, 0, 0);
+
+  
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            transform.Translate(0, 6, 0);
+        }
+
+    }
+}
