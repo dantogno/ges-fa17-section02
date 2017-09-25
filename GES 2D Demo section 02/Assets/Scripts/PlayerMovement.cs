664343177fc8fa2b29d0d47b95cea271e784be7d
David Antognoli <vebulidbih@msn.com>
2017-09-25 15:16:11 -0500

    refactored movment and jump to use physics system

--- GES 2D Demo section 02/Assets/Scripts/PlayerMovement.cs
+++ GES 2D Demo section 02/Assets/Scripts/PlayerMovement.cs
@@ -3,32 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-	// Use this for initialization
-	void Start ()
+    [SerializeField]
+    private float speed = 8;
+
+    [SerializeField]
+    private float jumpHeight = 6;
+
+    private bool isOnGround;
+
+    private Rigidbody2D myRigidbody2D;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Debug.Log("Called from Start.");  
+        isOnGround = true;
+    }
 
-        // This code "teleports" my game object to a new position
-        //transform.position = new Vector3(3,0,0);
+    private void MyNewFunction(Collider2D collision)
+    {
 
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();  
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
 
         Debug.Log("horizontal input: " + horizontalInput);
 
-        float speed = 0.2f;
 
-        transform.Translate(speed * horizontalInput, 0, 0);
-
-  
+        myRigidbody2D.velocity =
+            new Vector2(speed * horizontalInput, myRigidbody2D.velocity.y);
 
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isOnGround)
         {
-            transform.Translate(0, 6, 0);
+            myRigidbody2D.velocity =
+                new Vector2(myRigidbody2D.velocity.x, jumpHeight);
+
+            isOnGround = false;
         }
 
     }
