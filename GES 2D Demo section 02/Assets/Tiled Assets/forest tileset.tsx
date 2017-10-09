d8e876e5dcf9ef11dd03c87059519a245cc8c30c
David Antognoli <vebulidbih@msn.com>
2017-10-09 15:18:21 -0500

    added tiled and tiled2unity, and a sample tilemap to my level

---
+++ GES 2D Demo section 02/Assets/Tiled Assets/forest tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="forest tileset" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="../Sprites and Textures/Tile sheets/ForestSheet.png" width="272" height="128"/>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
