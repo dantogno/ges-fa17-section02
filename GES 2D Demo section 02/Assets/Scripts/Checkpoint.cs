827385238c11372c68a20aed2aac49417ca520fe
David Antognoli <David.Antognoli@iam.local>
2017-11-13 15:13:32 -0600

    in class progress

--- GES 2D Demo section 02/Assets/Scripts/Checkpoint.cs
+++ GES 2D Demo section 02/Assets/Scripts/Checkpoint.cs
@@ -6,11 +6,51 @@ public class Checkpoint : MonoBehaviour
 {
     public static Checkpoint currentCheckpoint;
 
+    [SerializeField]
+    private float activatedScale = 1.5f, deactivatedScale = 1;
+
+    [SerializeField]
+    private Color activatedColor, deactivatedColor;
+
+    private SpriteRenderer spriteRenderer;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        DeactivateCheckpoint();
+    }
+
+    private void DeactivateCheckpoint()
+    {
+        transform.localScale = Vector3.one * deactivatedScale;
+        spriteRenderer.color = deactivatedColor;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            currentCheckpoint = this;
+            bool playerTouchedNewCheckpoint = currentCheckpoint != this;
+
+            if (playerTouchedNewCheckpoint)
+            {                
+                if (currentCheckpoint != null)
+                {
+                    currentCheckpoint.DeactivateCheckpoint();
+                }
+                ActivateCheckpoint();
+            }
         }
     }
+
+    private void ActivateCheckpoint()
+    {
+        currentCheckpoint = this;
+        transform.localScale = Vector3.one * activatedScale;
+        spriteRenderer.color = activatedColor;
+    }
+
+
 }
---
+++ GES 2D Demo section 02/Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    string sceneToLoad;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        // This code gets called each frame player is inside trigger.
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetButtonDown("Action"))
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+
+}
